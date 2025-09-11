0b81295ccf0ef82cf54fcb752209e34e446f0b36
Andy Weng <andyweng257@gmail.com>
2025-09-11 07:23:24 -0700

    Update testdatatypes.py

--- testdatatypes.py
+++ testdatatypes.py
@@ -1,5 +1,60 @@
-x = "this is a thing"
-y= x.split()
-z = y[0]
-print(y)
-print(z)
+# usersentence = input(" Please input sentence: ")
+# usersplit = usersentence.split()
+# userwrdnm = len(usersplit)
+# print(f"Your sentence: {usersentence} /////  has {userwrdnm} words")
+
+
+# userinput = float(input("Input your number"))
+# int(userinput)
+# calc = userinput % 2
+# if calc == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+
+# def bad(userbill):
+#         print(f" Your total is ${userbill}")
+
+# def Okay(userbill):
+#     usertotal = userbill + (userbill * .15 )
+#     print(f" Your total bill is ${usertotal}.")
+
+# def Good(userbill):
+#     usertotal = userbill + (userbill * .20)
+#     print(f" Your total is ${usertotal}.")
+
+# def Great(userbill):
+#     usertotal = userbill +(userbill * .25)
+#     print(f"Your total is ${usertotal}.")
+
+# userbill = float(input("How much was your bill?: "))
+# while True:
+
+#     userinput = input("How was the service? (Bad/Okay/Good/Great: ").upper()
+#     if userinput == "BAD":
+#         bad(userbill)
+#         break
+#     elif userinput =="OKAY":
+#          Okay(userbill)
+#          break
+#     elif userinput =="GOOD":
+#          Good(userbill)
+#          break
+#     elif userinput == "GREAT":
+#          Great(userbill)
+#          break
+#     else:
+#          print(" YOU HAVE TO EITHER TYPE BAD, OKAY, GOOD, OR GREAT!!")
+
+import math
+
+usernum1 = int(input("Please input a whole number: "))
+usernum2 = int(input("Please input a second whole number: "))
+
+gcd = math.gcd(usernum1, usernum2)
+print(f" The gcf of {usernum1} and {usernum2} is {gcd}")
+
+
+
